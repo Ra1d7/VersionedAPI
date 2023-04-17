569414b85231f97f18902d72094bc709c97c412c
Raid7 Raid7 <thmrgb8@gmail.com>
2023-04-18 02:53:09 +0300

    added information for version pages

--- VersionedAPI/Program.cs
+++ VersionedAPI/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Versioning;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,7 +9,27 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(opts => {
+
+var Title = "Versioned API";
+var Description = "This is an example of what a versioned API would be like";
+var terms = new Uri("https://localhost:7221");
+var Contact = new OpenApiContact()
+{
+    Email = "thmrgb2@gmail.com",
+    Url = terms,
+    Name = "Moath Tar (Raid7)"
+};
+var license = new OpenApiLicense()
+{
+    Name = "My test License",
+    Url = terms,
+};
+
+    opts.SwaggerDoc("v2", new OpenApiInfo() { Title = $"{Title} v2", Contact = Contact, Description = Description, TermsOfService = terms,License = license, Version = "v2" });
+    opts.SwaggerDoc("v1", new OpenApiInfo() { Title = $"{Title} v1", Contact = Contact, Description = Description,TermsOfService = terms,License = license,Version = "v1"});
+
+});
 builder.Services.AddApiVersioning(opts => { 
 
     opts.AssumeDefaultVersionWhenUnspecified = true;
@@ -28,8 +49,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI(opts => {
 
+        opts.SwaggerEndpoint("/swagger/v2/swagger.json", "v2");
         opts.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
-        opts.SwaggerEndpoint("/swaggers/v2/swagger.json", "v2");
     
     });
 }
