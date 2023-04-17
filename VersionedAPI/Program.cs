817ac5b9cde775dad37ada5e672f8ab72fe39cd6
Raid7 Raid7 <thmrgb8@gmail.com>
2023-04-18 01:48:29 +0300

    test

--- VersionedAPI/Program.cs
+++ VersionedAPI/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddApiVersioning(opts => { 
+
+    opts.AssumeDefaultVersionWhenUnspecified = true;
+    opts.DefaultApiVersion = new ApiVersion(1, 0);
+    opts.ReportApiVersions = true;
+});
 
 var app = builder.Build();
 
