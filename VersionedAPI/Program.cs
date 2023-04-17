8e31c6a1960015ed29ada7e5c0ffe1c46d30333e
Raid7 Raid7 <thmrgb8@gmail.com>
2023-04-18 02:17:27 +0300

    added more versioning

--- VersionedAPI/Program.cs
+++ VersionedAPI/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Versioning;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,6 +14,11 @@ builder.Services.AddApiVersioning(opts => {
     opts.AssumeDefaultVersionWhenUnspecified = true;
     opts.DefaultApiVersion = new ApiVersion(1, 0);
     opts.ReportApiVersions = true;
+});
+builder.Services.AddVersionedApiExplorer(opts => {
+    opts.AssumeDefaultVersionWhenUnspecified = true;
+    opts.DefaultApiVersion = new ApiVersion(16, 0);
+
 });
 
 var app = builder.Build();
